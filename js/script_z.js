a47621e46d23c091ef8496867c6fcce039d097ae
ovvladimir <ov.vladimir@mail.ru>
2023-05-14 14:13:26 +1000

    -

--- js/script_z.js
+++ js/script_z.js
@@ -1,18 +1,18 @@
-    var correctAnswersCount = 0;
+let correctAnswersCount = 0;
 
-    function askQuestion(textBoxId, answer) {
-        var userAnswer = document.getElementById(textBoxId).value
-        if (userAnswer.toLowerCase() == answer) {
-            correctAnswersCount++;
-        }
+function askQuestion(textBoxId, answer) {
+    let userAnswer = document.getElementById(textBoxId).value
+    if (userAnswer.toLowerCase() == answer) {
+        correctAnswersCount++;
     }
-    function playPuzzle() {
-        correctAnswersCount = 0;
-        askQuestion("userAnswer1", "капуста");
-        askQuestion("userAnswer2", "лампа");
-        askQuestion("userAnswer2", "лампочка");
-        askQuestion("userAnswer3", "замок");
+}
+function playPuzzle() {
+    correctAnswersCount = 0;
+    askQuestion("userAnswer1", "капуста");
+    askQuestion("userAnswer2", "лампа");
+    askQuestion("userAnswer2", "лампочка");
+    askQuestion("userAnswer3", "замок");
 
     document.getElementById("result").innerHTML = 
-    "<strong><p>Количество правильных ответов: " + correctAnswersCount + "</p></strong>"
-    }
+    "<p class='text-primary'>Количество правильных ответов: <span class='text-danger'>" + correctAnswersCount + "</span></p>"
+}
