19814ee860f5a6f72969a91ce34d79a6b52a6033
ovvladimir <ov.vladimir@mail.ru>
2023-05-14 16:32:13 +1000

    -

--- js/script_u.js
+++ js/script_u.js
@@ -2,7 +2,7 @@ function play() {
     const element = document.getElementById("result");
     let answer = parseInt(Math.random() * 100); // [0 - 1] * 100
     let userAnswer;
-    console.log(answer) // подсказка в консоле или F12
+    // console.log(answer) // подсказка в консоле или F12
 
     for (let user=1; user<=2; user++) {
         userAnswer = prompt("Игрок №" + user + ", введите число от 0 до 100");
@@ -19,7 +19,7 @@ function play() {
             alert("Ваш ответ слишком маленький");
         } else if (userAnswer == answer) {
             alert("Игрок №" + user + ". Поздравляю! Вы угадали!");
-            element.innerHTML = "<p>Хорошая игра</p>";
+            element.innerHTML = "<p>Хорошая игра!</p>";
             break;
         } else {
             alert("Вы ошиблись. Необходимо ввести число.");
