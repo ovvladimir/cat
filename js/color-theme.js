3a97eef3f4d3091ae6b518f9f649b56e036864a6
ovvladimir <ov.vladimir@mail.ru>
2023-05-17 21:47:31 +1000

    -

--- js/color-theme.js
+++ js/color-theme.js
@@ -1,27 +1,34 @@
-const showActiveTheme = (theme, focus = false) => {
-  // console.log(theme);
-  const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
-  const svgOfActiveBtn = btnToActive.querySelector('i use').getAttribute('href');
-  document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-    element.classList.remove('active');
-    element.setAttribute('aria-pressed', 'false');
-  })
+(() => {
+  const currentTheme = localStorage.getItem("theme")
+  const getPreferredTheme = () => { return currentTheme }
+  const setTheme = function (theme) {
+    const formControl = document.querySelector(".main")
+    if (theme === "light") {formControl.style.backgroundColor = "#f8f9fa"; formControl.style.color = "#212529"}
+    else {formControl.style.backgroundColor = "#212529"; formControl.style.color = "#f8f9fa"}
+  }
+  setTheme(currentTheme)
+  // console.log(currentTheme)
 
-  btnToActive.classList.add('active');
-  btnToActive.setAttribute('aria-pressed', 'true');
-  document.getElementById("icon-active").className = "bi " + svgOfActiveBtn + " mb-3 fs-2";
-}
+  const showActiveTheme = (theme) => {
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+    const svgOfActiveBtn = btnToActive.querySelector('i use').getAttribute('href')
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+      element.classList.remove('active')
+      element.setAttribute('aria-pressed', 'false')
+    })
 
-window.addEventListener('DOMContentLoaded', () => {
+    btnToActive.classList.add('active')
+    btnToActive.setAttribute('aria-pressed', 'true')
+    document.getElementById("icon-active").className = "bi " + svgOfActiveBtn + " mb-3 fs-2"
+  }
+
+  showActiveTheme(getPreferredTheme())
   document.querySelectorAll('[data-bs-theme-value]').forEach(toggle => {
     toggle.addEventListener('click', () => {
-      const theme = toggle.getAttribute('data-bs-theme-value');
-      // localStorage.setItem('theme', theme);
-
-      showActiveTheme(theme, true)
-      const formControl = document.querySelector('.main');
-      if (theme === 'light') {formControl.style.backgroundColor = '#f8f9fa'; formControl.style.color = '#212529';}
-      else if (theme === 'dark') {formControl.style.backgroundColor = '#212529'; formControl.style.color = '#f8f9fa';};
+      const theme = toggle.getAttribute('data-bs-theme-value')
+      localStorage.setItem('theme', theme)
+      setTheme(theme)
+      showActiveTheme(theme)
     })
   })
-})
+})()
