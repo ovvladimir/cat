d8628cfbab7bf1a00fd045bde25bcb6cbfe3fd04
ovvladimir <ov.vladimir@mail.ru>
2023-07-30 00:42:31 +1000

    -

--- js/main_jquery.js
+++ js/main_jquery.js
@@ -1,16 +1,16 @@
 // Прокрутка страницы вверх и вниз jquery
 let forAll = 100
-if ($(window).scroll() > forAll) $("#scrollUp").fadeIn('slow');
+if ($(window).scrollTop() > forAll) $("#scrollUp").fadeIn('slow');
 $(window).scroll(function() {
-    if ($(window).scroll() < forAll) $("#scrollUp").fadeOut('slow');
+    if ($(window).scrollTop() < forAll) $("#scrollUp").fadeOut('slow');
     else $("#scrollUp").fadeIn('slow');
 });
 
-if ($(window).scroll() + $(window).height() + forAll < $(document).height()) $("#scrollDown").fadeIn('slow');
+if ($(window).scrollTop() + $(window).height() < $(document).height() - forAll) $("#scrollDown").fadeIn('slow');
 $(window).scroll(function() {
-    if ($(window).scroll() + $(window).height() + forAll > $(document).height()) $("#scrollDown").fadeOut('slow');
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - forAll) $("#scrollDown").fadeOut('slow');
     else $("#scrollDown").fadeIn('slow');
 });
 
 $("#scrollUp").click(function(){$("html,body").animate({scrollTop: 0}, forAll)});
-$("#scrollDown").click(function(){$("html,body").animate({scrollTop: $(document).height() - $(window).height()}, forAll)});
+$("#scrollDown").click(function(){$("html,body").animate({scrollTop: $(document).height()}, forAll)});
